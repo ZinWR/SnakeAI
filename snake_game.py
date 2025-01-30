c04fd9d683e5d1cfb6c6919f2e3613165d98e8b0
QuanN <jeanquan98@gmail.com>
2025-01-29 19:06:39 -0800

    creating small snake_game

---
+++ snake_game.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+
+pygame.init()
+
+class SnakeGame:
+    def __init__(self, width=640 , height=480):
+        self.width = width
+        self.height = height
+        # Display Init
+
+        # Game State Init
+
+    def play_step(self):
+        pass
+
+if __name__ == '__main__':
+    game = SnakeGame()
+
+    # Game Loop
+    while True:
+        game.play_step()
+
+        # Break if Game Over
+        pass
+
+    pygame.quit()
