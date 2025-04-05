fixed move function with wrong index setup

game.py
@@ -153,11 +153,11 @@ class SnakeGameAI:
         
         elif np.array_equal(action, [0,1,0]):
             next_idx = (idx + 1) % 4
-            new_direction = clock_wise[idx] # right turn r -> d -> l -> u
+            new_direction = clock_wise[next_idx] # right turn r -> d -> l -> u
         
         else: # [0,0,1]
             next_idx = (idx - 1) % 4
-            new_direction = clock_wise[idx] # left turn r -> u -> l -> d
+            new_direction = clock_wise[next_idx] # left turn r -> u -> l -> d
 
         self.direction = new_direction
 
@@ -174,7 +174,7 @@ class SnakeGameAI:
 
         self.head = Point(x,y)
 
-# Main Game
+# Main Game Without Agent
 # if __name__ == '__main__':
 #     game = SnakeGameAI()
 